fbae87b7b9eba48b2039e750993c0eb293e0756d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-01-04 12:17:17 +0500

    Work Complete

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var profileMenu = document.getElementById("profileMenu");
 function toggleMenu() {
   profileMenu.classList.toggle("open-menu");
 }
+
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity() {
+  sideActivity.classList.toggle("open-activity");
+
+  if (sideActivity.classList.contains("open-activity")) {
+    moreLink.innerHTML = "Show less <b>-</b>";
+  } else {
+    moreLink.innerHTML = "Show more <b>+</b>";
+  }
+}
